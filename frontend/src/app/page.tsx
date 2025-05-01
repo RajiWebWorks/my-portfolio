7bd1bb70670919a94740b8c0b953a57cc2ca394d
Rajeshwari Kumar <rajirdx39@gmail.com>
2025-05-01 17:50:11 +0530

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import HeroSection from "@/components/sections/HeroSection";
 import AboutSection from "@/components/sections/AboutSection";
 import SkillsSection from "@/components/sections/SkillsSection";
